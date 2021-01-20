bf36a5f0fc955938e9c71f80df9f1ec9867e6c6b
Terrance Cazy <terrancecazy@MacBook-Pro.lan>
2021-01-20 16:21:46 -0600

    complete steps 1-2

---
+++ src/components/Topics/EvenAndOdd.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+
+
+class EvenAndOdds extends Component{
+  render(){
+    return(
+      <p>EvenAndOdd Component</p>
+    )
+  }
+}
+
+
+export default EvenAndOdds;
