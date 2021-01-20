5a63925b218f9f2f8d45dedcf86b9c0d2e0bbfc7
Terrance Cazy <terrancecazy@MacBook-Pro.lan>
2021-01-20 16:04:36 -0600

    initial commit

---
+++ src/components/TopicBrowser/TopicBrowser.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+
+
+class TopicBrowser extends React.Component {
+  render() {
+    return <p>Hello, World</p>;
+  }
+}
+
+
+export default TopicBrowser;
